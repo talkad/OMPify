63c32f58cc45e914297116290a8a537c5f489737
Tal Kadosh <talkad@post.bgu.ac.il>
2022-12-03 12:22:02 +0200

    removed redundant code

--- visualization/visualize_metadata.py
+++ visualization/visualize_metadata.py
@@ -9,7 +9,6 @@ def visualize_keywords(metadata_path):
     plot pi graph of all keywords
 
     {'c-plus-plus': 81, 'mpi': 487, 'openmp': 1585, 'cuda': 341, 'opencl': 81, 'cpp': 272, 'gpu': 74, 'parallel-computing': 322, 'fortran': 57, 'c': 325, 'parallel-programming': 237, 'hpc': 115, 'pthreads': 88, 'openmp-parallelization': 111, 'openmpi': 105, 'multithreading': 95, 'parallel': 142, 'python': 59, 'simd': 51, 'high-performance-computing': 65}
-    {'C': 3556, 'Python': 164, 'C++': 3442, 'Shell': 157, 'Cuda': 135, 'Fortran': 204} - missed_out = 571
     '''
     keywords = []
     top_keywords = {}
@@ -76,10 +75,13 @@ def count_users_info(metadata_path):
     '''
     Get company, location and email countings
 
+    amount of national labs 33
+    {'lanl.gov': 10, 'llnl.gov': 6, 'pnnl.gov': 5, 'olcf.ornl.gov': 3, 'bnl.gov': 3, 'sandia.gov': 2, 'mcs.anl.gov': 1, 'usgs.gov': 1, 'fnal.gov': 1, 'ornl.gov': 1}
     ({'Google': 17, 'Carnegie Mellon University': 15, 'Intel': 15, 'NVIDIA': 16, 'Microsoft': 39, 'Amazon': 11}, 
-    {'Spain': 20, 'London': 32, 'Germany': 43, 'Boston, MA': 12, 'New York': 27, 'Chicago, IL': 11, 'China': 13, 'Singapore': 26, 'Berlin, Germany': 19, 'Paris, France': 18, 'Tokyo': 13, 'Poland': 29, 'Stockholm': 18, 'Seattle, WA': 20, 'UK': 15, 'Taiwan': 16, 'Beijing, China': 11, 'Santa Clara, CA': 12, 'Pittsburgh, PA': 13, 'Portugal': 12, 'France': 48, 'Berlin': 17, 'San Francisco': 19, 'Japan': 13, 'USA': 24, 'San Francisco, CA': 18, 'London, UK': 18, 'Italy': 31, 'Berkeley, CA': 12, 'Edinburgh, UK': 13, 'Austin, TX': 17, 'Moscow': 32, 'United States': 45, 'Dublin, Ireland': 23, 'Egypt': 15, 'Brazil': 60, 'Bangalore': 29, 'Bangalore, India': 15, 'Hsinchu, Taiwan': 13, 'Moscow, Russia': 31, 'Canada': 17, 'Greece': 29, 'Athens, Greece': 36, 'Barcelona': 23, 'Paris': 27, 'Melbourne': 13, 'Los Angeles': 12, 'India': 61, 'Brasil': 13, 'Bristol, UK': 14, 'Munich, Germany': 12, 'Thessaloniki, Greece': 17, 'New York, NY': 19, 'Israel': 14, 'Hyderabad': 18, 'Hong Kong': 15, 'United Kingdom': 14, 'Hyderabad, India': 15, 'Russia': 29, 'Beijing': 16, 'Dallas, TX': 12, 'United States of America': 18, 'Madrid, Spain': 11, 'Tokyo, Japan': 13, 'Istanbul': 11, 'Redmond, WA': 11, 'Karachi, Pakistan': 11},
-     {'gmail.com': 1763, 'hotmail.com': 67, 'qq.com': 30, 'intel.com': 24, 'live.com': 11, 'outlook.com': 57, '163.com': 19, 'mail.ru': 18, 'yahoo.com': 21, 'googlegroups.com': 18, 'yandex.ru': 15, 'protonmail.com': 14, 'epcc.ed.ac.uk': 11})
+    {'Spain': 20, 'London': 32, 'Germany': 43, 'Boston, MA': 12, 'New York': 27, 'Chicago, IL': 11, 'China': 13, 'Singapore': 26, 'Berlin, Germany': 19, 'Paris, France': 18, 'Tokyo': 13, 'Poland': 29, 'Stockholm': 18, 'Seattle, WA': 20, 'UK': 15, 'Taiwan': 16, 'Beijing, China': 11, 'Santa Clara, CA': 12, 'Pittsburgh, PA': 13, 'Portugal': 12, 'France': 48, 'Berlin': 17, 'San Francisco': 19, 'Japan': 13, 'USA': 24, 'San Francisco, CA': 18, 'London, UK': 18, 'Italy': 31, 'Berkeley, CA': 12, 'Edinburgh, UK': 13, 'Austin, TX': 17, 'Moscow': 32, 'United States': 45, 'Dublin, Ireland': 23, 'Egypt': 15, 'Brazil': 60, 'Bangalore': 29, 'Bangalore, India': 15, 'Hsinchu, Taiwan': 13, 'Moscow, Russia': 31, 'Canada': 17, 'Greece': 29, 'Athens, Greece': 36, 'Barcelona': 23, 'Paris': 27, 'Melbourne': 13, 'Los Angeles': 12, 'India': 61, 'Brasil': 13, 'Bristol, UK': 14, 'Munich, Germany': 12, 'Thessaloniki, Greece': 17, 'New York, NY': 19, 'Israel': 14, 'Hyderabad': 18, 'Hong Kong': 15, 'United Kingdom': 14, 'Hyderabad, India': 15, 'Russia': 29, 'Beijing': 16, 'Dallas, TX': 12, 'United States of America': 18, 'Madrid, Spain': 11, 'Tokyo, Japan': 13, 'Istanbul': 11, 'Redmond, WA': 11, 'Karachi, Pakistan': 11}, 
+    {'gmail': 1763, 'protonmail': 17, 'hotmail': 74, 'cs': 12, 'qq': 30, 'intel': 24, 'live': 15, 'outlook': 65, '163': 19, 'mail': 29, 'yahoo': 31, 'googlegroups': 18, 'yandex': 15, 'epcc': 11})
     '''
+    national_labs = []
     keywords = []
     companies, locations, emails = [], [], []
     top_companies, top_locations, top_emails = {}, {}, {}
@@ -90,16 +92,22 @@ def count_users_info(metadata_path):
         for metadata in data:
             companies.append(metadata['company'])
             locations.append(metadata['location'])
-            email = '' if metadata['email'] is None else metadata['email']
-            emails.append(email[email.find('@')+1:])
+            email = '' if metadata['email'] is None else metadata['email']            
+            email = email[email.find('@')+1:]
+            emails.append(email[:email.find('.')])
+
+            if metadata['email'] is not None and '.gov' in metadata['email']:
+                national_labs.append(email)
 
     for top, count in zip([top_companies, top_locations, top_emails], [Counter(companies), Counter(locations), Counter(emails)]):
         for k, v in count.items():
             if k is not None and v > 10 and len(k) > 0:
                 top[k] = v
 
+    print(f'amount of national labs {len(national_labs)}')
+    print(national_labs)
     return top_companies, top_locations, top_emails
 
 
-print(count_lang('/home/talkad/Downloads/thesis/data_gathering_script/visualization/metadata.pickle'))
-# print(count_users_info('/home/talkad/Downloads/thesis/data_gathering_script/visualization/metadata_user.pickle'))
+# print(count_lang('/home/talkad/Downloads/thesis/data_gathering_script/visualization/metadata.pickle'))
+print(count_users_info('/home/talkad/Downloads/thesis/data_gathering_script/visualization/metadata_user.pickle'))
